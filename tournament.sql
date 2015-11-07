7e7d26a3e99874df25f72196af2bb7f0c71f364e
Marvin Fuller <mfuller0@bellsouth.net>
2015-11-07 16:53:37 -0500

    cleaned up syntax line 44

--- tournament.sql
+++ tournament.sql
@@ -41,7 +41,7 @@ CREATE VIEW view_standings AS
     SELECT * FROM players ORDER BY wins DESC;
 ALTER VIEW view_standings OWNER TO web;
 
-
+Swiss_
 DROP VIEW IF EXISTS view_players;
 CREATE VIEW view_players AS
     SELECT id,pl_name AS Player FROM players ORDER BY id ASC;
